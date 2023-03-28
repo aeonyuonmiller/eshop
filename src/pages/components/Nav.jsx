1d0c23ca3d2f76faa50aa491bd4cf8ef870f4896
aeonyuonmiller <aym1@mail.com>
2023-03-28 23:06:59 +0200

    minor updates

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -3,25 +3,28 @@ import { m } from "framer-motion";
 
 function Nav () {
     const group = {
-        hide: { x: 1, transition:{ staggerChildren: 0.3, } },
-        show: { x: 0, transition:{ staggerChildren: 0.3, }}
+        hide: { y: 10 },
+        show: { y: 0, transition:{ staggerChildren: 0.1 }}
       }
   
       const item = {
-        hide: { y: 20, opacity: 0 },
-        show: { y: 0, opacity: 1, transition:{ ease: [.64, .62, .23, .99], duration: .8 }}
+        hide: { y: "100%", opacity: 0, filter: "blur(18px)" },
+        show: { y: 0, opacity: 1, filter: "blur(0)", transition:{ ease: [.98,0,.2,1.01], duration: .4 }}
     }
 
   return (
       <nav>
         <m.ul initial="hide" animate="show" variants={group}>
-              <m.li variants={item}>returns</m.li>
-              <m.li variants={item}>shipping</m.li>
-              <m.li variants={item}>privacy</m.li>
-              <m.li variants={item}>instagram</m.li>
-              <m.li variants={item}>youtube</m.li>
-              <m.li variants={item}>cart</m.li>
+            <m.li variants={item}>returns</m.li>
+            <m.li variants={item}>shipping</m.li>
+            <m.li variants={item}>privacy</m.li>
+            <m.li variants={item}>instagram</m.li>
+            <m.li variants={item}>youtube</m.li>
         </m.ul>
+        
+        <m.span initial={{ opacity: 0, scale:0.5, y:50, filter: "blur(18px)" }} animate={{ opacity:1, scale:1, y:0, filter: "blur(0)" }} transition={{ delay:0.7, duration:0.4 }}>
+            <div className="cart-icon"></div>
+        </m.span>
       </nav>
   )
 }
