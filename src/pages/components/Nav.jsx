d7fb709c0ef394f4de8ba15ff6f4a0a9a1d9e98b
aeonyuonmiller <aym1@mail.com>
2023-03-28 22:47:02 +0200

    added staggered nav animation

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { m } from "framer-motion";
 
-const Nav = () => {
+function Nav () {
     const group = {
         hide: { x: 1, transition:{ staggerChildren: 0.3, } },
         show: { x: 0, transition:{ staggerChildren: 0.3, }}
@@ -11,16 +11,16 @@ const Nav = () => {
         hide: { y: 20, opacity: 0 },
         show: { y: 0, opacity: 1, transition:{ ease: [.64, .62, .23, .99], duration: .8 }}
     }
-  
 
   return (
       <nav>
-        <m.ul initial="hide" animate="show" variant={group}>
-              <m.li variant={item}>returns</m.li>
-              <m.li variant={item}>shipping</m.li>
-              <m.li variant={item}>privacy</m.li>
-              <m.li variant={item}>instagram</m.li>
-              <m.li variant={item}>youtube</m.li>
+        <m.ul initial="hide" animate="show" variants={group}>
+              <m.li variants={item}>returns</m.li>
+              <m.li variants={item}>shipping</m.li>
+              <m.li variants={item}>privacy</m.li>
+              <m.li variants={item}>instagram</m.li>
+              <m.li variants={item}>youtube</m.li>
+              <m.li variants={item}>cart</m.li>
         </m.ul>
       </nav>
   )
