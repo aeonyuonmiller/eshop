58101cf1775c39e411689d250d922429797045df
aeonyuonmiller <aym1@mail.com>
2023-03-30 16:27:46 +0200

    smoother interactions

--- src/pages/components/Layout.jsx
+++ src/pages/components/Layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { m } from "framer-motion";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, bgColor }) => {
+
+  const bg = {
+      backgroundColor: `${bgColor}`,
+    };
+  
   return (
-    <m.div className='layout' initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }} exit={{ opacity: 0, transition:{duration: .8, delay: .4} }}>
+    <m.div className='layout' style={bg} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 }}} exit={{ opacity: 0, transition:{ duration: .8, delay: .4}}}>
       {children}
     </m.div>
   )
--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -5,25 +5,26 @@ import Link from 'next/link'
 function Nav () {
     const group = {
         hide: { },
-        show: { transition:{ staggerChildren:0.2, delayChildren:0.5 }}
+        show: { transition:{ staggerChildren:0.1, delayChildren:0.5 }}
       }
   
       const item = {
-        hide: { y: "100%", opacity: 0, filter: "blur(6px)" },
-        show: { y: "0%", opacity: 1, filter: "blur(0)", transition:{ ease: [.98,0,.2,1.01], duration: .6 }}
+        hide: { y: 20, opacity: 0, filter: "blur(6px)" },
+        show: { y: 0, opacity: 1, filter: "blur(0)", transition:{ ease: [.98,0,.2,1.01], duration: .6 }}
     }
 
   return (
       <nav>
-        <m.div initial="hide" animate="show" variants={group}>
-            <m.span><Link tabIndex={2} className="underline" href="/returns" variants={item}>returns</Link></m.span>
-            <m.span><Link tabIndex={2} className="underline" href="/shipping" variants={item}>shipping</Link></m.span>
-            <m.span><Link tabIndex={2} className="underline" href="/privacy" variants={item}>privacy</Link></m.span>
+      <m.div initial="hide" animate="show" variants={group}>
+            <m.span variants={item}><Link tabIndex={2} className="underline" href="/">home</Link></m.span>
+            <m.span variants={item}><Link tabIndex={2} className="underline" href="/returns">returns</Link></m.span>
+            <m.span variants={item}><Link tabIndex={2} className="underline" href="/shipping">shipping</Link></m.span>
+            <m.span variants={item}><Link tabIndex={2} className="underline" href="/privacy">privacy</Link></m.span>
             <m.a tabIndex={2} className="underline" variants={item}>instagram</m.a>
             <m.a tabIndex={2} className="underline" variants={item}>youtube</m.a>
         </m.div>
         
-        <m.span><Link href="/" tabIndex={1} initial={{ opacity: 0, scale:0, y:50, filter: "blur(18px)" }} animate={{ opacity:1, scale:1, y:0, filter: "blur(0)" }} transition={{ delay:.7, duration:0.4 }}>
+        <m.span><Link href="/" tabIndex={1} initial={{ opacity: 0, scale:0, y:50 }} animate={{ opacity:1, scale:1, y:0 }} transition={{ delay:.8, duration:0.4 }}>
         <div className="cart-icon"></div>
         </Link>
         </m.span>
