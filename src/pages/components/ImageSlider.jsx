2b2ca97462749b052cc9a758b050981885cf7161
aeonyuonmiller <aym1@mail.com>
2023-04-04 18:31:09 +0200

    updated hover

--- src/pages/components/ImageSlider.jsx
+++ src/pages/components/ImageSlider.jsx
@@ -11,16 +11,18 @@ function ImageSlider() {
   
       const item = {
         hide: { y: 20, opacity: 0, filter: "blur(6px)" },
+        leave: {scale:1,objectPosition: "0% center"},
         hover: { scale: 1.1, objectPosition: "100% center", transition: { ease: [.98, 0, .2, 1.01], duration: 1 } },
         show: { y: 0, opacity: 1, objectPosition: "0% center", filter: "blur(0)", transition:{ ease: [.98,0,.2,1.01], duration: .6 }}
-    }
+      }
+
 
     return (
     <m.div className="slider" initial="hide" animate="show" exit="exit" variants={group} data-mouse-down-at="0" data-prev-percentage="0">
-        <m.img variants={item} className="image" draggable="false" src="./cube.png" whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
-        <m.img variants={item} className="image" draggable="false" src="./glas.png" whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
-        <m.img variants={item} className="image" draggable="false" src="./lava.png" whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
-        <m.img variants={item} className="image" draggable="false" src="./mirror.png" whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
+        <m.img variants={item} className="image" draggable="false" src="./cube.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type: "spring", damping: 18 } }} />
+        <m.img variants={item} className="image" draggable="false" src="./glas.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
+        <m.img variants={item} className="image" draggable="false" src="./lava.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
+        <m.img variants={item} className="image" draggable="false" src="./mirror.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
     </m.div>
   );
 }
