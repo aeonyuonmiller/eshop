ac09af144bd346ed6653cc99514943c2bbd3dcf4
aeonyuonmiller <aym1@mail.com>
2023-04-04 22:04:34 +0200

    faster enter animation

--- src/pages/components/ImageSlider.jsx
+++ src/pages/components/ImageSlider.jsx
@@ -1,12 +1,15 @@
+import { useRef } from "react";
 import { m, useScroll } from "framer-motion";
 
 function ImageSlider() {
-  const { scrollYProgress } = useScroll()
+  const ref = useRef(null);
+  const { scrollXProgress } = useScroll({ container: ref });
+
   
   const group = {
     hide: {},
     hover: {},
-    show: { transition: { staggerChildren: 0.1, delayChildren: 1 }},
+    show: { transition: { staggerChildren: 0.1, delayChildren: .7 }},
     exit: { opacity: 0 },
   }
   
@@ -17,13 +20,27 @@ function ImageSlider() {
     show: { y: 0, opacity: 1, objectPosition: "0% center", filter: "blur(0)", transition:{ ease: [.98,0,.2,1.01], duration: .6 }}
   }
 
-    return (
-    <m.div className="slider" initial="hide" animate="show" exit="exit" variants={group} data-mouse-down-at="0" data-prev-percentage="0">
+  return (
+    <>
+    <svg id="progress" width="50" height="50" viewBox="0 0 100 100">
+      <circle cx="50" cy="50" r="30" pathLength="1" />
+      <m.circle
+        cx="50"
+        cy="50"
+        r="30"
+        pathLength="0"
+        className="indicator"
+        style={{ pathLength: scrollXProgress }}
+      />
+    </svg>
+
+    <m.div ref={ref} className="slider" initial="hide" animate="show" exit="exit" variants={group} data-mouse-down-at="0" data-prev-percentage="0">
         <m.img variants={item} className="image" draggable="false" src="./cube.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type: "spring", damping: 18 } }} />
         <m.img variants={item} className="image" draggable="false" src="./glas.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
         <m.img variants={item} className="image" draggable="false" src="./lava.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
         <m.img variants={item} className="image" draggable="false" src="./mirror.png" animate={{ scale: 1, objectPosition: "0% center", transition: { type:"spring", damping: 18 }}} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type:"spring", damping: 18 }}}/>
-    </m.div>
+      </m.div>
+      </>
   );
 }
 
--- src/pages/components/icons/CloseButton.jsx
+++ src/pages/components/icons/CloseButton.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import { m } from "framer-motion";
 
 const close = () => {
     return (
-        <Link className='close' href="/" tabIndex={1}>
-            close
-        </Link>
+        <m.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+            <Link className='close' href="/" tabIndex={1}>
+                close
+            </Link>
+        </m.div>
+
   )
 }
 
