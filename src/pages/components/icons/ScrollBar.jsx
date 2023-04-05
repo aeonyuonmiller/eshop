47a3c1816891dd9897af76f0d974c00966b5da9c
aeonyuonmiller <aym1@mail.com>
2023-04-05 02:35:37 +0200

    fine-adjustments

--- src/pages/components/icons/ScrollBar.jsx
+++ src/pages/components/icons/ScrollBar.jsx
@@ -7,7 +7,7 @@ function ScrollBar() {
         target: targetRef,
         offset: ["start end", "end start"],
     })
-    const scrolled = useTransform(scrollXProgress, [0,1], [0, 203])
+    const scrolled = useTransform(scrollXProgress, [0,1], [0, 204])
 
   return (
       <m.svg
@@ -23,12 +23,12 @@ function ScrollBar() {
         exit={{ pathOffset: 1, x: 20, transition:{ delay:0, duration: .6, type: "easeIn" }}}
         stroke="#000"
         strokeLinecap="round"
-        strokeOpacity="0.2"
+        strokeOpacity="0.4"
         strokeWidth="9"
         d="M4 5h291"
       ></m.path>
           <m.path
-            style={{ x: scrolled }}
+              style={{ x: scrolled }}
         initial={{ pathLength: 0, opacity: 0, pathOffset: 0, offsetPath: 0 }}
         animate={{ pathLength:.3, pathOffset: 0, opacity:[0,1,1], offsetPath: 0, transition:{ delay:1.5, duration: .7, type: "easeOut" }}}
         exit={{ pathLength:.3, x: 20, strokeWidth:1, pathOffset: 1, offsetPath: 1, transition:{ delay:0, duration: .6, type: "easeIn" }}}
