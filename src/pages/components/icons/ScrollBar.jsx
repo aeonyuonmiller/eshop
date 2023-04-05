ceb81a657b39c1d624061189f15695d37af50716
aeonyuonmiller <aym1@mail.com>
2023-04-05 02:29:41 +0200

    scrollbar working

--- src/pages/components/icons/ScrollBar.jsx
+++ src/pages/components/icons/ScrollBar.jsx
@@ -1,13 +1,13 @@
 import React, {useRef, forwardRef} from "react";
 import { m, useScroll, useTransform } from "framer-motion"
 
-function ScrollBar({ref}) {
+function ScrollBar() {
     const targetRef = useRef(null);
     const { scrollXProgress } = useScroll({
         target: targetRef,
         offset: ["start end", "end start"],
     })
-    const opacity = useTransform(scrollXProgress, [0,0.5], [1, 0])
+    const scrolled = useTransform(scrollXProgress, [0,1], [0, 203])
 
   return (
       <m.svg
@@ -19,18 +19,19 @@ function ScrollBar({ref}) {
     >
       <m.path
         initial={{ pathLength: 0, opacity: 0, pathOffset: 0 }}
-        animate={{ pathLength:1, opacity:[0,1,1], pathOffset: 0, transition:{ delay:2, duration: 1, type: "easeOut" }}}
-        exit={{ pathOffset: 1, transition:{ delay:0, duration: .6, type: "easeIn" }}}
+        animate={{ pathLength:1, opacity:[0,1,1], pathOffset: 0, transition:{ delay:1.5, duration: .7, type: "easeOut" }}}
+        exit={{ pathOffset: 1, x: 20, transition:{ delay:0, duration: .6, type: "easeIn" }}}
         stroke="#000"
         strokeLinecap="round"
         strokeOpacity="0.2"
         strokeWidth="9"
         d="M4 5h291"
       ></m.path>
-        <m.path
+          <m.path
+            style={{ x: scrolled }}
         initial={{ pathLength: 0, opacity: 0, pathOffset: 0, offsetPath: 0 }}
-        animate={{ pathLength:.3, pathOffset: 0, opacity:[0,1,1], offsetPath: 0, transition:{ delay:2, duration: 1, type: "easeOut" }}}
-        exit={{ pathLength:.3, pathOffset: 1, offsetPath: 1, transition:{ delay:0, duration: .6, type: "easeIn" }}}
+        animate={{ pathLength:.3, pathOffset: 0, opacity:[0,1,1], offsetPath: 0, transition:{ delay:1.5, duration: .7, type: "easeOut" }}}
+        exit={{ pathLength:.3, x: 20, strokeWidth:1, pathOffset: 1, offsetPath: 1, transition:{ delay:0, duration: .6, type: "easeIn" }}}
         stroke="#ffffff"
         strokeLinecap="round"
         strokeWidth="3"
