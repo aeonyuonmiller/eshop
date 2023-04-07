ae67fc6bde976c5ce94c8178743620f3a2873072
aeonyuonmiller <aym1@mail.com>
2023-04-07 18:46:38 +0200

    added animations

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -11,11 +11,17 @@ const Card = ({ url = "", src = "", title = "", price = "" }) => {
     leave: {scale:1, objectPosition: "0% center"},
   }
     
+  const copy = {
+    hide: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1, transition:{ ease: [.98,0,.2,1.01], duration: .6 }},
+    leave: {scale:1, objectPosition: "0% center"},
+  }
+    
     return (
         <div className='card'>
             <Link tabIndex="1" aria-labelledby={title} prefetch href={url}>
                 <m.img loading="lazy" className="image" variants={item} draggable="false" src={src} animate={{ scale: 1, objectPosition: "0% center", transition: { type: "spring", damping: 18 } }} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type: "spring", damping: 18 } }} />
-                <div className='content'><span>{title}</span><strong>{price}</strong></div>
+                <m.div variants={copy} className='content'><span>{title}</span><strong>{price}</strong></m.div>
             </Link>
         </div>
     )
