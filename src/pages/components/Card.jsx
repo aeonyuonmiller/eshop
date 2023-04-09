7cd57f24e58cda6de6e349636eea6fb21e35f6d9
aeonyuonmiller <aym1@mail.com>
2023-04-09 19:01:47 +0200

    reduced items of InView-animation

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -21,10 +21,10 @@ const Card = ({ url = "", src = "", title = "", price = "" }) => {
         <m.section className="card" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }}>
           <Link tabIndex="1" aria-labelledby={title} prefetch href={url}>
             <m.img loading="lazy" className="image" variants={item} draggable="false" src={src} animate={{ scale: 1, objectPosition: "0% center", transition: { type: "spring", damping: 18 } }} whileHover={{ scale: 1.1, objectPosition: "100% center", transition: { type: "spring", damping: 18 } }} />
-            <div className='content'>
-              <m.span initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0, transition:{ ease: [.98,0,.2,1.01], duration: .6, delay: .1 }}}>{title}</m.span>
-              <m.strong initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0, transition:{ ease: [.98,0,.2,1.01], duration: .6, delay: .1 }}}>{price}</m.strong>
-            </div>
+            <m.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0, transition:{ ease: [.98,0,.2,1.01], duration: .6, delay: .1 }}} className='content'>
+              <span>{title}</span>
+              <strong>{price}</strong>
+            </m.div>
           </Link>
         </m.section>
     )
