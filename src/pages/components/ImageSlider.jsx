410a4e1928ec382eaf914060c0e58eafbb42c68d
aeonyuonmiller <aym1@mail.com>
2023-04-11 21:24:18 +0200

    mobile: styling .scrollbar

--- src/pages/components/ImageSlider.jsx
+++ src/pages/components/ImageSlider.jsx
@@ -6,11 +6,15 @@ function ImageSlider({children}) {
   const ref = useRef(null);
 
   return (
+    
     <>
-    <ScrollBar ref={ref} />
-    <m.div ref={ref} className="slider" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-      {children}
-    </m.div>
+      <ScrollBar ref={ref} />
+      
+      <div className="slider-container" ref={ref}>
+         <m.div className="slider" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+          {children}
+        </m.div>
+      </div>
     </>
   );
 }
