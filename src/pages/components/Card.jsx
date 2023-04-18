6fe194e221770f9021de94dff2c2e64b61d666f1
aeonyuonmiller <aym1@mail.com>
2023-04-19 01:39:37 +0200

    updated values

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -16,8 +16,8 @@ const Card = ({ url = "", src = "", title = "", price = "", desc = "" }) => {
   const x = useMotionValue(200);
   const y = useMotionValue(200);
 
-  const rotateX = useTransform(x, [0, 400], [-40, 40]);
-  const rotateY = useTransform(y, [0, 400], [-40, 40]);
+  const moveX = useTransform(x, [0, 400], [-40, 40]);
+  const moveY = useTransform(y, [0, 400], [-15, 15]);
 
   function handleMouse(event) {
       const rect = event.currentTarget.getBoundingClientRect();
@@ -61,7 +61,7 @@ const Card = ({ url = "", src = "", title = "", price = "", desc = "" }) => {
         Add to cart
       </m.button>
         <Link tabIndex="1" aria-labelledby={title} prefetch href={url} scroll={false}>
-          <m.img loading="lazy" style={{ x: rotateX, y: rotateY }} className="image" variants={item} src={src} transition="transition" animate="show" whileHover="hover" exit="exit" draggable="false" />
+          <m.img loading="lazy" style={{ x: moveX, y: moveY }} className="image" variants={item} src={src} transition="transition" animate="show" whileHover="hover" exit="exit" draggable="false" />
           <m.div className='content' viewport={{ amount: 1 }} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0, transition:{ ease: [.98,0,.2,1.01], duration: .6, delay: .1 }}}>
             <span>{title}</span>
             <span>{price}</span>
